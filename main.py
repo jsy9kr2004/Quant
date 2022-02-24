d288631559ca3525471d63602ef280cf7a329261
sungyong_cho <jsy9kr2004@gmail.com>
2022-02-25 01:52:24 +0900

    List 추가 새로운 common 함수 추가 (_only_one)

--- main.py
+++ main.py
@@ -39,9 +39,10 @@ def get_fmp_es():
     symbol = stock["symbol"]
 
 
-def get_fmp_data_by_list(parent_path, main_url, data_list, extra_url):
+def get_fmp_data_by_list(parent_path, main_url, data_list, extra_url, v4_flag=False, is_symbol=True):
     # brief : 순차적으로 넣고자 하는 값을 url에 포함시켜서 돌려줌
     # input : parent_path(나의 상위 폴더), main_url(url의 main 값), data_list (url에 넣고자하는 list), extra_url(뒤에 나머지)
+    #            v4_flag(v4 url 형식을 사용할지에 대한 flag)
     # output : none
     # example 1 : /api/v3/discounted-cash-flow/AAPL?apikey=***
     #               get_fmp_data_by_list("./data". "dicounted-cash-flow", symbol_list, "")
@@ -60,13 +61,30 @@ def get_fmp_data_by_list(parent_path, main_url, data_list, extra_url):
         elem = data_list[i]
         if not os.path.isfile(path + "/{}.csv".format(elem)):
             print('Creating File "{}/{}.csv" ...'.format(path, elem))
-            json_profile = pd.read_json(fmp_url + "{}/{}?{}apikey={}".format(main_url, elem, extra_url, api_key))
-            json_profile.to_csv(path+"/{}.csv".format(elem), na_rep='NaN')
+            if v4_flag is True:
+                json_data = pd.read_json(fmp_url + "{}/{}?{}apikey={}".format(main_url, elem, extra_url, api_key))
+            else:
+                json_data = pd.read_json(fmp_url + "{}/{}?{}apikey={}".format(main_url, elem, extra_url, api_key))
+            json_data.to_csv(path+"/{}.csv".format(elem), na_rep='NaN')
         else:
             if cre_flag is True:
                 # 새로 만드는 경우, 이미 csv가 있다는 건 stock list와 delisted list에 중복 값이 있는 상황 (Duplicate)
                 # 리스트에 중복값이 왜 들어가게 되었는지 반드시 확인이 필요함. (가정이 깨짐)
                 print('[ERROR] Already Exist "{}/{}.csv"'.format(path, elem))
+
+
+def get_fmp_data_only_one(path, main_url, v4_flag=False):
+    # brief : 하나의 값만 읽어오고자 할 때 사용됨
+    # input : path(저장하고자 하는 폴더 폴더), main_url(url의 main 값), extra_url(뒤에 나머지 url)
+    #            v4_flag(v4 url 형식을 사용할지에 대한 flag)
+    # output : none
+    # example 1 :  /api/v3/financial-statement-symbol-lists
+    #                   get_fmp_data_only_one("./data", "financial-statement-symbol-lists", "")
+    #                   ./data 폴더 아래에 financial-statement-symbol-lists.csv 파일을 만듦
+    if not os.path.isfile("{}/{}.csv".format(path, main_url)):
+        print('Creating File "{}/{}.csv" ...'.format(path, main_url))
+        json_data = pd.read_json(fmp_url + "{}?apikey={}".format(main_url, api_key))
+        json_data.to_csv("{}/{}.csv".format(path, main_url), na_rep='NaN')
 
 
 def get_fmp():
@@ -111,9 +129,10 @@ def get_fmp_sy():
     stock = pd.concat([available_traded, delisted_stock], ignore_index=True)
     symbol = stock["symbol"]
 
+    # Financial Statements List
+    get_fmp_data_only_one("./data", "financial-statement-symbol-lists")
     # Create Financial Statement Folder & Files (income, balance-sheet, cash-flow)
     create_folder("./data/financial-statement/")
-
     # TODO FMP 유료 결제 후 분기에서 연간 데이터를 분기 데이터로 바꾸기 위해 url_2 값(limit=120&)을 변경해주어야 함
     # Income Statement
     get_fmp_data_by_list("./data/financial-statement/", "income-statement", symbol, "limit=120&")
@@ -121,8 +140,65 @@ def get_fmp_sy():
     get_fmp_data_by_list("./data/financial-statement/", "balance-sheet-statement", symbol, "limit=120&")
     # Cash Flow Statement
     get_fmp_data_by_list("./data/financial-statement/", "cash-flow-statement", symbol, "limit=120&")
-
-    # 이 사이에 많은 함수들이 들어와야 함 (by RULE)
+    # Income Statement as reported
+    get_fmp_data_by_list("./data/", "income-statement-as-reported", symbol, "period=quarter&limit=50&")
+    # Balance Sheet Statement as reported
+    get_fmp_data_by_list("./data/", "balance-sheet-statement-as-reported", symbol, "period=quarter&limit=50&")
+    # Cash Flow Statement as reported
+    get_fmp_data_by_list("./data/", "cash-flow-statement-as-reported", symbol, "period=quarter&limit=50&")
+    # Full Financial Statement as reported
+    get_fmp_data_by_list("./data/", "financial-statement-full-as-reported", symbol, "period=quarter&")
+    # International Filings
+    # TODO 여기에 올 내용은 미국장 이외의 장의 symbol이 필요. list만 넣어주면 돌아가나 아직 필요성 의문
+    # get_fmp_data_by_list("./data/", "income-statement", symbol, "limit=100&")
+    # List of dates and links
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Annual Reports on Form 10-K
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Quarterly Earnings Reports
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Shares Float
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # SEC RSS Feeds
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Earning Call Transcript
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # SEC Filings
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Rss feed 8K (Important Events)
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Company Notes due New
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Company Financial Ratios
+    get_fmp_data_by_list("./data/", "ratios", symbol, "period=quarter&limit=140&")
+    # Stock Financial scores
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Company Enterprise Value
+    get_fmp_data_by_list("./data/", "enterprise-values", symbol, "period=quarter&limit=130&")
+    # Financial Statements Growth
+    get_fmp_data_by_list("./data/", "income-statement-growth", symbol, "limit=40&")
+    get_fmp_data_by_list("./data/", "balance-sheet-statement-growth", symbol, "limit=40&")
+    get_fmp_data_by_list("./data/", "cash-flow-statement-growth", symbol, "limit=40&")
+    # Company Key Metrics
+    get_fmp_data_by_list("./data/", "key-metrics", symbol, "period=quarter&limit=130&")
+    # Company Financial Growth
+    get_fmp_data_by_list("./data/", "financial-growth", symbol, "period=quarter&limit=80&")
+    # Company Rating
+    get_fmp_data_by_list("./data/", "historical-rating", symbol, "limit=140&")
+    # Company DCF
+    get_fmp_data_by_list("./data/", "discounted-cash-flow", symbol, "")
+    # Institutional Stock Ownership
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Earnings Calendar
+    get_fmp_data_by_list("./data/", "earning_calendar", symbol, "limit=80&")
+    # Earnings Calendar Confirmed
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Stock Split Calendar
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Dividend Calendar
+    get_fmp_data_by_list("./data/", "", symbol, "")
+    # Economic Calendar
+    get_fmp_data_by_list("./data/", "", symbol, "")
     # Company Profile
     get_fmp_data_by_list("./data/", "profile", symbol, "")
 
